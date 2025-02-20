d85edcd664979cce2716223326149f249a756d8f
Polepalli Vishnu Sai <119359369+vishnu7899@users.noreply.github.com>
2025-02-20 11:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (37.16%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int first_digit=0;
+        int second_digit=1;
+        int next_digit=0;
+
+        
+        for(int i=2;i<=n;i++){
+        
+         next_digit=first_digit+second_digit;
+         first_digit= second_digit;
+         second_digit=next_digit;    
+        
+        }
+        return next_digit;
+    }
+}
